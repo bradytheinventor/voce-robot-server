eaea3a6bd0a7446cf55c0b62a9564e5b8ac8d812
bradytheinventor <bcra1234@gmail.com>
2017-05-25 15:22:59 -0500

    Added AverageEncoderPIDSource
    Added a PIDSource that returns the average of two encoder values.

---
+++ src/org/usfirst/frc/team2823/robot/AverageEncoderPIDSource.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team2823.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class AverageEncoderPIDSource implements PIDSource {
+	
+	Encoder l;
+	Encoder r;
+	
+	public AverageEncoderPIDSource(Encoder l, Encoder r) {
+		this.l = l;
+		this.r = r;
+	}
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return PIDSourceType.kDisplacement;
+	}
+
+	@Override
+	public double pidGet() {
+		return (l.getDistance() + r.getDistance()) / 2;
+	}
+}
