305fc3422e505ca0754c8a1aa4bff3a37e5f31a4
bradytheinventor <bcra1234@gmail.com>
2017-05-25 15:30:07 -0500

    Added motor controllers
    Added drive motor controllers and drive methods.

--- src/org/usfirst/frc/team2823/robot/Robot.java
+++ src/org/usfirst/frc/team2823/robot/Robot.java
@@ -2,16 +2,28 @@ package org.usfirst.frc.team2823.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	String prevCommand = "FAIL";
 	Servo servo;
 	
+	Talon rTalon;
+	Spark rSpark;
+	Talon lTalon;
+	Spark lSpark;
+	
 	@Override
 	public void robotInit() {
 		servo = new Servo(9);
 		servo.set(0.0);
+		
+		rTalon = new Talon(0);
+		rSpark = new Spark(1);
+		lTalon = new Talon(2);
+		lSpark = new Spark(3);
 		
 		SmartDashboard.putBoolean("New Command", false);
 	}
@@ -53,5 +65,15 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void testPeriodic() {
 		
+	}
+	
+	public void driveLeft(double p) {
+		lTalon.set(p);
+		lSpark.set(p);
+	}
+	
+	public void driveRight(double p) {
+		rTalon.set(-p);
+		rSpark.set(-p);
 	}
 }
